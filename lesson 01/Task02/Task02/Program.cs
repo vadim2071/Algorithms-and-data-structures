3bb24221fef43aa2d8c9818565e07a08633de2e5
vadim2071 <vadim207138@gmail.com>
2021-06-28 20:55:19 +0300

    End Task02

---
+++ lesson 01/Task02/Task02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+        //Вычислите асимптотическую сложность функции из примера ниже. 
+        // K - размер массива
+        // 2 - количество операции внутри последнего цикла
+        // сложность  = O(K * K * K * 2)
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)
+                    {
+                        int y = 0;
+
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+
