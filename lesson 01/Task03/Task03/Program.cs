263c18b10c2183b959a7cd848a1b04faa5020171
vadim2071 <vadim207138@gmail.com>
2021-06-29 20:39:26 +0300

    Final Task03

--- lesson 01/Task03/Task03/Program.cs
+++ lesson 01/Task03/Task03/Program.cs
@@ -8,17 +8,15 @@ namespace Task03
         {
             Console.WriteLine("Введите число для вычисления Числа Фибоначчи");
             int n = Convert.ToInt32(Console.ReadLine());
-            int n_Rec = FibonachiRecursion(0, 1, n - 2);
+            int n_Rec = FibonachiRecursion(0, 1, n - 1);
             int n_Cycle = FibonachiCycle(n);
             Console.WriteLine("Число Фибоначчи через рекусию {0}\nЧисло Фибоначчи через цикл {1}", n_Rec, n_Cycle);
 
             //метод вычисления Фиббоначи через рекурсию
             static int FibonachiRecursion(int a, int b, int num)
             {
-                a = a + b;
-                num--;
-                if (num > 0) a = FibonachiRecursion(b, a, num);
-                return a;
+                if (num > 0) return FibonachiRecursion(b, a + b, num - 1);
+                else return b;
             }
 
             //метод вычисления Фиббоначи через цикл
@@ -26,22 +24,13 @@ namespace Task03
             {
                 int a = 0, b = 1, c = 1;
 
-                for (int i = 0; i < n - 2; i++)
+                for (int i = 0; i < n - 1; i++)
                 {
                     c = a + b;
                     a = b;
                     b = c;
                 }
                 return c;
-
-                /*while (n - 2 > 0)
-                {
-                    c = a + b;
-                    a = b;
-                    b = c;
-                    n--;
-                }
-                return c;*/
             }
         }
     }
