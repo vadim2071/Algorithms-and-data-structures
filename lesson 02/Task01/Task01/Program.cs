7b1fabe69fca608acaad4d41f37ac4720d1d7fa3
vadim2071 <vadim207138@gmail.com>
2021-07-02 01:39:44 +0300

    End Task02 Lesson02

---
+++ lesson 02/Task01/Task01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public class Node : ILinkedList
+        {
+            public int Value { get; set; }
+            public Node NextNode { get; set; }
+            public Node PrevNode { get; set; }
+
+            public int GetCount()
+            {
+                int i = 0;
+                while (NextNode != null)
+                {
+                    i++;
+                    NextNode = NextNode.NextNode;
+                }
+                return i;
+            }
+
+            public void AddNode(int value)
+            {
+
+            }
+
+            public void AddNodeAfter(Node node,int value)
+            {
+
+            }
+
+            public void RemoveNode(int index)
+            {
+
+            }
+
+            public void RemoveNode(Node node)
+            {
+
+            }
+
+            public Node FindNode(int searchValue)
+            {
+                return ;
+            }
+
+        }
+
+        //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+        public interface ILinkedList
+        {
+            int GetCount(); // возвращает количество элементов в списке
+            void AddNode(int value);  // добавляет новый элемент списка
+            void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+            void RemoveNode(int index); // удаляет элемент по порядковому номеру
+            void RemoveNode(Node node); // удаляет указанный элемент
+            Node FindNode(int searchValue); // ищет элемент по его значению
+        }
+    }
+}
+
---
+++ lesson 02/Task02/Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Требуется написать функцию бинарного поиска, посчитать его асимптотическую сложность и проверить работоспособность функции.
+// нужное число найдется максимально за O(log(N)) (N- диапазон поиска, количесвто значений, которое может принять искомое число)
+
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] test = new int [] {0, 10, 25, 50, 71, 99, 100 };
+            int lenght = test.Length;
+            
+            for (int i = 0; i < lenght; i++) Console.WriteLine($"Найдено за {BinarySerch(0, 100, test[i])} шагов, правильное число {test[i]}");
+
+            static int BinarySerch(int min, int max, int num)
+            {
+                int average = 0;
+                int count = 0;
+                while (average != num)
+                {
+                    average = (min + max) / 2;
+                    if (num < average) max = average-1;
+                    else if (num > average) min = average+1;
+                    count++;
+                }
+                Console.WriteLine($"\n загаданное число {average}");
+                return count;
+            }
+        }
+    }
+}
