7fbfd69e38ff57997ed30aa132ec39f9933f0ee6
vadim2071 <vadim207138@gmail.com>
2021-07-07 01:18:59 +0300

    Task01 Lesson03

--- lesson 03/Task01/Task01/Program.cs
+++ lesson 03/Task01/Task01/Program.cs
@@ -17,28 +17,53 @@ namespace Task01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PointClassFloat dot1 = new PointClassFloat();
+            dot1.X = 555161;
+            dot1.Y = 5151654;
+            PointClassFloat dot2 = new PointClassFloat();
+            dot2.X = 5,55566161;
+            dot2.Y = 515112654;
 
-            static void DistReferencFloat() // Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float).
+            Console.WriteLine(DistReferencFloat(dot1, dot2));
+
+            static float DistReferencFloat(PointClassFloat dot1, PointClassFloat dot2) // Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float).
+            {
+                var i = Math.Sqrt((dot1.X - dot2.X) * (dot1.X - dot2.X) + (dot1.Y - dot2.Y) * (dot1.Y - dot2.Y));
+            return Math.Sqrt((dot1.X - dot2.X) * (dot1.X - dot2.X) + (dot1.Y - dot2.Y) * (dot1.Y - dot2.Y));
+            }
+
+            static void DistMeaningfulFloat(PointStructFloat dot1, PointStructFloat dot2) // Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float).
             {
 
             }
 
-            static void DistMeaningfulFloat() // Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float).
+            static double DistMeaningfulDouble(PointStructDouble dot1, PointStructDouble dot2) // Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double).
+            {
+                return Math.Sqrt((dot1.X - dot2.X) * (dot1.X - dot2.X) + (dot1.Y - dot2.Y) * (dot1.Y - dot2.Y));
+            }
+
+            static void DistMeaningfulFloatNoSqure(PointStructFloat dot1, PointStructFloat dot2) // Метод расчёта дистанции без квадратного корня со значимым типом (PointStruct — координаты типа float).
             {
 
             }
 
-            static void DistMeaningfulDouble() // Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double).
-            {
+        }
+        public class PointClassFloat
+        {
+            public float X;
+            public float Y;
+        }
 
-            }
+        public struct PointStructFloat
+        {
+            public float X;
+            public float Y;
+        }
 
-            static void DistMeaningfulFloatNoSqure() // Метод расчёта дистанции без квадратного корня со значимым типом (PointStruct — координаты типа float).
-            {
-
-            }
-
+        public struct PointStructDouble
+        {
+            public double X;
+            public double Y;
         }
     }
 }
