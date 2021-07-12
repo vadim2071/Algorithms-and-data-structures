dfe7169f3226970a8234f172ba260fc494d5f475
vadim2071 <vadim207138@gmail.com>
2021-07-12 21:25:31 +0300

    Next

--- lesson 04/Task 02/Task 02/Program.cs
+++ lesson 04/Task 02/Task 02/Program.cs
@@ -6,7 +6,37 @@ namespace Task_02
     {
         static void Main(string[] args)
         {
+            //Тесты
+            //создаем дерево
+            //                      18
+            //                  /           \
+            //                 10            22    
+            //               /    \        /     \            
+            //            11        13     77    88           
+            //            /\        /\            /\  
+            //           5  7     44   55       1    17         
+            //           /\              \                   
+            //          8  4              23                                      
+
             Node tree = new Node { Data = 1, Left = null, Right = null, Parent = null };
+            tree.AddItem(10);
+            tree.AddItem(22);
+            tree.Left.AddItem(11);
+            tree.Right.AddItem(13);
+            tree.GetNodeByValue(11).AddItem(5);
+            tree.GetNodeByValue(11).AddItem(7);
+            tree.GetNodeByValue(5).AddItem(8);
+            tree.GetNodeByValue(5).AddItem(4);
+            tree.GetNodeByValue(13).AddItem(44);
+            tree.GetNodeByValue(13).AddItem(55);
+            tree.GetNodeByValue(55).AddItem(23);
+            tree.GetNodeByValue(22).AddItem(77);
+            tree.GetNodeByValue(22).AddItem(88);
+            tree.GetNodeByValue(88).AddItem(1);
+            tree.GetNodeByValue(88).AddItem(17);
+
+            Node find = new Node();
+            Console.WriteLine($"")
         }
         public class Node : ITree
         {
@@ -15,7 +45,7 @@ namespace Task_02
             public Node Right { get; set; }
             public Node Parent { get; set; }
 
-            public Node GetRoot() // получение корня дерева
+            public Node GetRoot() // получение корня дерева (хотя можно было бы и хранить в классе)
             {
                 Node find = this;
                 while (find.Parent != null) find = find.Parent;
@@ -23,28 +53,49 @@ namespace Task_02
             }
             public void AddItem(int value)
             {
-
+                Node newNode = new Node {Data = value, Left = null, Right = null, Parent = this };
+                if (this.Left == null) this.Left = newNode;
+                else if (this.Right == null) this.Right = newNode;
             }
-            public void RemoveItem(int value)
+            public void RemoveItem(int value) // удалить узел по значению
             {
-                Node del = GetRoot();
-                while (del.Data != value)
+                Node del = FindNode(GetRoot(), value);
+                if (del.Parent != null) // если есть верхний элемент (владелец удаляемого элемента)
                 {
-
+                    if (del.Parent.Left == del) del.Parent.Left = null; // если удаляемый элемент является левой веткой владельца, уадаляем всю ветку 
+                    else if (del.Parent.Right == del) del.Parent.Right = null; // если удаляемый элемент является правой веткой владельца, уадаляем всю ветку 
+                                                                                //удалить элемент из цепочки не можем, может возникнуть ситуация когда у 
+                                                                                //удаляемго элемента могут быть 2 ветки, а мы можем аписать ссылку только на одну. 
                 }
+                del.Parent = null;
+                del.Left = null;
+                del.Right = null;
+                    
             }
-            public Node GetNodeByValue(int value)
+            public Node GetNodeByValue(int value) //получить узел дерева по значению
             {
-                Node find = GetRoot();
-                while (find.Data != value)
-                {
-                   if (find.Left != null) find = find.Left;
-                }
-
-
+                return FindNode(GetRoot(), value); //находим корень дерева, запускаем обход дерева. Возвращает найденный элемент или null если не найдено. 
             }
-            public void PrintTree()
+
+            public Node FindNode(Node find, int value) // рекурсивый метод обхода дерева. Возвращает найденный элемент или null если не найдено. 
             {
+                Node returnNode = null;
+                if (find.Data == value) return find;
+                else if (find.Left != null) returnNode = FindNode(find.Left, value);
+                if (find.Right != null & returnNode != null) returnNode = FindNode(find.Right, value);
+                return returnNode;
+            }
+            public void PrintTree() //вывести дерево в консоль
+            {
+                Console.WriteLine($"{PrintNext(GetRoot())}");
+            }
+            public string PrintNext(Node node)
+            {
+                string PrintString = $"({node.Data})";
+                if (node.Left != null & node.Right != null) PrintString = PrintString + "(" + PrintNext(node.Left) + "  " + PrintNext(node.Right) + ")";
+                else if (node.Left != null & node.Right == null) PrintString = PrintString + "(" + PrintNext(node.Left) + "  " + ")";
+                else if (node.Left == null & node.Right != null) PrintString = PrintString + "(" + "  " + PrintNext(node.Right) + ")";
+                return PrintString;
 
             }
              
@@ -57,77 +108,5 @@ namespace Task_02
             Node GetNodeByValue(int value); //получить узел дерева по значению
             void PrintTree(); //вывести дерево в консоль
         }
-
-
-
-
-        public class TreeNode
-        {
-            public int Value { get; set; }
-            public TreeNode LeftChild { get; set; }
-            public TreeNode RightChild { get; set; }
-
-
-            
-
-
-            public override bool Equals(object obj)
-            {
-                var node = obj as TreeNode;
-
-                if (node == null)
-                    return false;
-
-                return node.Value == Value;
-            }
-        }
-
-
-        public static class TreeHelper
-        {
-            public static NodeInfo[] GetTreeInLine(ITree tree)
-            {
-                var bufer = new Queue<NodeInfo>();
-                var returnArray = new List<NodeInfo>();
-                var root = new NodeInfo() { Node = tree.GetRoot() };
-                bufer.Enqueue(root);
-
-                while (bufer.Count != 0)
-                {
-                    var element = bufer.Dequeue();
-                    returnArray.Add(element);
-
-                    var depth = element.Depth + 1;
-
-                    if (element.Node.LeftChild != null)
-                    {
-                        var left = new NodeInfo()
-                        {
-                            Node = element.Node.LeftChild,
-                            Depth = depth,
-                        };
-                        bufer.Enqueue(left);
-                    }
-                    if (element.Node.RightChild != null)
-                    {
-                        var right = new NodeInfo()
-                        {
-                            Node = element.Node.RightChild,
-                            Depth = depth,
-                        };
-                        bufer.Enqueue(right);
-                    }
-                }
-
-                return returnArray.ToArray();
-            }
-        }
-
-        public class NodeInfo
-        {
-            public int Depth { get; set; }
-            public TreeNode Node { get; set; }
-        }
-
     }
 }
