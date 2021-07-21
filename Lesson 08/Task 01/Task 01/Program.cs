6468eb7ae9231965c3f4e18d4c0975bd9fdb560d
vadim2071 <vadim207138@gmail.com>
2021-07-22 00:10:04 +0300

    Task01 Lesson08 start

---
+++ Lesson 08/Task 01/Task 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            void quickSort(int[] arr, int first, int last)  //Метод быстрой сортировки
+            {
+                int element; //опорный элемент
+                int lenght = arr.Length;
+                element = arr[lenght / 2]; //берем опорный элемент из середины массива
+
+
+
+            }
+        }
+    }
+}
